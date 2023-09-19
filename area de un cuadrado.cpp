18a6bc5428fc327dcb781c7f16f1b31db7fe0cf5
Daniel Blancas <143759289+SrQuack0737@users.noreply.github.com>
2023-09-18 21:38:09 -0600

    Add files via upload
    Area Cuadrado v1

---
+++ area de un cuadrado.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+//MENU DE OPERACIONES 
+int n1, area;
+  printf("\nArea de cuadrado  ");
+  printf("\nIntroduzca lado   (entero): ");
+  scanf("%d", &n1);
+
+  area = n1 * n1;
+  printf("\nEl area de el cuadrado es: %d", area);
+  return 0;
+}
