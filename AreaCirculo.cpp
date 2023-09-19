ab8b905b3ae23efda2a136368dfef4fa618cb78b
Sr.Quack 0737 <blancasisaac847@gmail.com>
2023-09-18 21:58:37 -0600

    Se anexan 3 programas con 1era version

---
+++ AreaCirculo.cpp
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    // MENU DE OPERACIONES 
+    int n1;
+    float areaCirculo;
+    float pi = 3.1416;
+
+    return 0;
+}
---
+++ AreaTriangulo.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+//MENU DE OPERACIONES 
+
+  printf("\nArea de Triangulo  ");
+  printf("\nIntroduzca base   (entero): ");
+  scanf("%d", &n1);
+  printf("\nIntroduzca altura   (entero): ");
+  scanf("%d", &n2);
+
+  areaTriangulo= (n1 * n2) /2;
+  printf("\nEl area de el cuadrado es: %d", areaTriangulo);
+  return 0;
+}
